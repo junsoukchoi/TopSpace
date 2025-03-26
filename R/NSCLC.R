a092e5d73d15d61f3d882376c65a2c22e1cac75d
Junsouk Choi <jchoi@stat.tamu.edu>
2025-03-26 11:27:30 +0900

    Add the NSCLC dataset.

---
+++ R/NSCLC.R
@@ -0,0 +1,12 @@
+#' Non-small Cell Lung Cancer Dataset
+#'
+#' A dataset of multiplex immunohistochemistry images of tissue sections from 153 patients with non-small cell lung cancer. 
+#'
+#' @format A list 
+#' \describe{
+#'   \item{variable1}{Description of variable1}
+#'   \item{variable2}{Description of variable2}
+#'   \item{variable3}{Description of variable3}
+#' }
+#' @source 
+"NSCLC"
